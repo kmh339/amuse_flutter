65872fc0101293e227661466c221641a07eaa0a7
kmh <kmh339@gmail.com>
2020-04-14 16:37:58 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:amuse_flutter/authentication/authentication_bloc.dart';
+import 'package:amuse_flutter/user_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:amuse_flutter/authentication/bloc.dart';
+
+import 'package:http/http.dart' as http;
+
+void main() {
+  final UserRepository userRepository = UserRepository();
+  runApp(BlocProvider(
+    create: (context) => AuthenticationBloc(
+      userRepository: userRepository,
+      httpClient: http.Client(),
+    )..add(AppStarted()),
+    child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+
+      )
+    );
+  }
+}
---
+++ lib/user_repository.dart
@@ -0,0 +1,137 @@
+import 'dart:async';
+
+import 'package:meta/meta.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class UserRepository {
+  Future<String> authenticate({
+    @required String username,
+    @required String password,
+  }) async {
+    await Future.delayed(Duration(seconds: 1));
+    return 'token';
+  }
+
+  Future<void> deleteToken() async {
+    /// delete from keystore/keychain
+    final prefs = await SharedPreferences.getInstance();
+    prefs.remove("accessTokenWith");
+    return;
+  }
+
+  Future<void> persistToken(String token) async {
+    /// write to keystore/keychain
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('accessTokenWith', token);
+    return;
+  }
+
+  Future<bool> hasToken() async {
+    /// read from keystore/keychain
+    final prefs = await SharedPreferences.getInstance();
+    String accessToken = prefs.getString('accessTokenWith');
+    if (accessToken != null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<String> getAccessToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    String accessToken = prefs.getString('accessTokenWith');
+    return accessToken;
+  }
+
+  Future<void> persistNotipicationToken(String token) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('notipicationToken', token);
+    return;
+  }
+
+  Future<String> getNotificationToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    String notipicationToken = prefs.getString('notipicationToken');
+    return notipicationToken;
+  }
+
+
+  Future<String> getAuthenticate() async {
+    final prefs = await SharedPreferences.getInstance();
+    String username = prefs.getString('usernameWith');
+    return username;
+  }
+
+  Future<void> persistUsername(String username) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('usernameWith', username);
+    return;
+  }
+
+
+  Future<int> getStartAt() async {
+    final prefs = await SharedPreferences.getInstance();
+    int startAt = prefs.getInt('startatWith');
+    return startAt;
+  }
+
+  Future<void> persistStartAt(int startAt) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setInt('startatWith', startAt);
+    return;
+  }
+
+  Future<void> persistEmail(String email) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('emailWith', email);
+    return;
+  }
+
+  Future<String> getEmail() async {
+    final prefs = await SharedPreferences.getInstance();
+    String email = prefs.getString('emailWith');
+    return email;
+  }
+
+  Future<void> persistAddress(String address) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('addressWith', address);
+    return;
+  }
+
+  Future<void> persistDayDuration(int dayDuration) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setInt('dayDurationWith', dayDuration);
+    return;
+  }
+
+  Future<int> getDayDuration() async {
+    final prefs = await SharedPreferences.getInstance();
+    int dayDuration = prefs.getInt('dayDurationWith');
+    return dayDuration;
+  }
+
+  Future<void> signOut() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.remove("accessTokenWith");
+    prefs.remove('usernameWith');
+  }
+
+  Future<bool> isSignedIn() async {
+    final prefs = await SharedPreferences.getInstance();
+    String accessToken = prefs.getString('accessTokenWith');
+    if (accessToken != null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<String> getUser() async {
+    final prefs = await SharedPreferences.getInstance();
+    String username = prefs.getString('usernameWith');
+    return username;
+  }
+
+
+}
